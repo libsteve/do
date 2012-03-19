3791699b3fcf7c0322fc11f8deb093220813dd63
Steven Brunwasser <sbrunwasser@gmail.com>
2012-03-18 22:34:35 -0400

    worked on arg parser

--- do.py
+++ do.py
@@ -2,14 +2,17 @@ from optparse import OptionParser
 
 def args():
 	parser = OptionParser()
-	parser.add_option("-f", "--file", dest="filename", default="Makefile", help="specify the file to read from", metavar="FILE")
-	parser.add_option("-m", "--machine", dest="machinename", default="glados.cs.rit.edu", help="specify the machine to connect to", metavar="MACHINE")
+	parser.add_option("-f", "--file", dest="filename", default="", help="specify the file to read from")
+	parser.add_option("-m", "--machine", dest="machine", default="glados.cs.rit.edu", help="specify the machine to connect to")
+	parser.add_option("-u", "--user", dest="username", default="", help="specify the user")
+	parser.add_option("-p", "--password", dest="password", default="", help="specify the password")
 
 	(options, args) = parser.parse_args()
+	return (options, args)
 
-def read(filename):
-	f = open(filename, 'r')
-	if filename == "Makefile" or filename == "makefile":
-		readAsMakefile(f)
-	else:
-		readAsList(f)
+def main():
+	(options, args) = args()
+	filename = options.filename
+	machine = options.machine
+	username = options.username
+	password = options.password
